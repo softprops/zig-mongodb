3eb52923da204d60c1a0d4ca7e18991dc2ac55a0
softprops <d.tangren@gmail.com>
2024-06-06 01:39:04 -0400

    init to winit

---
+++ build.zig
@@ -0,0 +1,86 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const bson = b.dependency("bson", .{
+        .target = target,
+        .optimize = optimize,
+    }).module("bson");
+    const mongodb = b.createModule(.{
+        .root_source_file = b.path("src/root.zig"),
+        .imports = &.{
+            .{ .name = "bson", .module = bson },
+        },
+    });
+
+    try b.modules.put(b.dupe("mongodb"), mongodb);
+
+    // unit tests
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    unit_tests.root_module.addImport("bson", bson);
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+
+    // const benchmark_tests = b.addTest(.{
+    //     .root_source_file = b.path("src/bench.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    //     .filters = &.{"bench"},
+    // });
+
+    // const run_benchmark_tests = b.addRunArtifact(benchmark_tests);
+
+    // const benchmark_step = b.step("bench", "Run benchmark tests");
+    // benchmark_step.dependOn(&run_benchmark_tests.step);
+
+    inline for ([_]struct {
+        name: []const u8,
+        src: []const u8,
+    }{
+        .{ .name = "demo", .src = "examples/demo/main.zig" },
+    }) |example| {
+        const example_step = b.step(try std.fmt.allocPrint(
+            b.allocator,
+            "{s}-example",
+            .{example.name},
+        ), try std.fmt.allocPrint(
+            b.allocator,
+            "build the {s} example",
+            .{example.name},
+        ));
+
+        const example_run_step = b.step(try std.fmt.allocPrint(
+            b.allocator,
+            "run-{s}-example",
+            .{example.name},
+        ), try std.fmt.allocPrint(
+            b.allocator,
+            "run the {s} example",
+            .{example.name},
+        ));
+
+        var exe = b.addExecutable(.{
+            .name = example.name,
+            .root_source_file = b.path(example.src),
+            .target = target,
+            .optimize = optimize,
+        });
+        exe.root_module.addImport("mongodb", mongodb);
+
+        // run the artifact - depending on the example exe
+        const example_run = b.addRunArtifact(exe);
+        example_run_step.dependOn(&example_run.step);
+
+        // install the artifact - depending on the example exe
+        const example_build_step = b.addInstallArtifact(exe, .{});
+        example_step.dependOn(&example_build_step.step);
+    }
+}
---
+++ src/root.zig
@@ -0,0 +1,236 @@
+// todo: https://github.com/mongodb/specifications/tree/master/source
+// todo: https://www.mongodb.com/docs/manual/reference/mongodb-wire-protocol/
+const std = @import("std");
+const bson = @import("bson");
+const RawBson = @import("bson").types.RawBson;
+
+pub const SectionKind = enum(u8) {
+    body = 0,
+    doc_sequence = 1,
+    internal = 2,
+
+    fn toInt(self: @This()) u8 {
+        return @intFromEnum(self);
+    }
+
+    fn fromInt(i: u8) @This() {
+        return @enumFromInt(i);
+    }
+
+    pub fn format(
+        self: @This(),
+        comptime _: []const u8,
+        _: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        try writer.print("{s}", .{@tagName(self)});
+    }
+};
+
+// https://www.mongodb.com/docs/manual/reference/mongodb-wire-protocol/#opcodes
+pub const OpCode = enum(i32) {
+    /// Wraps other opcodes using compression
+    compressed = 2012,
+    /// Send a message using the standard format. Used for both client requests and database replies.
+    msg = 2013,
+    /// Reply to a client request. responseTo is set. Deprecated in MongoDB 5.0. Removed in MongoDB 5.1.
+    reply = 1,
+    /// Update document. Deprecated in MongoDB 5.0. Removed in MongoDB 5.1.
+    update = 2001,
+    /// Insert document. Deprecated in MongoDB 5.0. Removed in MongoDB 5.1.
+    insert = 2002,
+    /// Formerly used for OP_GET_BY_OID.
+    reserved = 2003,
+    /// Query a collection. . Deprecated in MongoDB 5.0. Removed in MongoDB 5.1.
+    query = 2004,
+    /// Get more data from a query. See Cursors..  Deprecated in MongoDB 5.0. Removed in MongoDB 5.1.
+    get_more = 2005,
+    /// Delete document. Deprecated in MongoDB 5.0. Removed in MongoDB 5.1.
+    delete = 2006,
+    /// Notify database that the client has finished with the cursor.  Deprecated in MongoDB 5.0. Removed in MongoDB 5.1.
+    kill_cursors = 2007,
+
+    fn toInt(self: @This()) i32 {
+        return @intFromEnum(self);
+    }
+
+    fn fromInt(i: i32) @This() {
+        return @enumFromInt(i);
+    }
+
+    pub fn format(
+        self: @This(),
+        comptime _: []const u8,
+        _: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        try writer.print("{s}", .{@tagName(self)});
+    }
+};
+
+// https://www.mongodb.com/docs/manual/reference/mongodb-wire-protocol/#standard-message-header
+pub const Header = struct {
+    /// The total size of the message in bytes. This total includes the 4 bytes that holds the message length.
+    message_len: i32,
+    /// A client or database-generated identifier that uniquely identifies the message
+    request_id: i32,
+    /// The requestID taken from the messages from the client.
+    response_to: i32,
+    /// Type of message. See Opcodes for details.
+    op_code: OpCode,
+};
+
+///  https://www.mongodb.com/docs/manual/legacy-opcodes/#op_query
+pub const OpQuery = struct {
+    header: Header,
+    flags: i32 = 0,
+    // null terminated
+    full_collection_name: []const u8,
+    number_to_skip: i32,
+    number_to_return: i32,
+    query: bson.types.Document,
+    return_fields_selector: ?bson.types.Document = null,
+};
+
+/// https://www.mongodb.com/docs/manual/legacy-opcodes/#op_reply
+pub const OpReply = struct {
+    header: Header,
+    response_flags: i32,
+    cursor_id: i64,
+    starting_from: i32,
+    number_returned: i32,
+    documents: []const bson.types.Document,
+};
+
+pub const ClientOptions = struct {
+    address: std.net.Address = .{
+        .in = std.net.Ip4Address.init(
+            .{ 127, 0, 0, 1 },
+            27017,
+        ),
+    },
+};
+
+pub const Client = struct {
+    allocator: std.mem.Allocator,
+    options: ClientOptions,
+
+    pub fn init(allocator: std.mem.Allocator, options: ClientOptions) @This() {
+        return .{ .allocator = allocator, .options = options };
+    }
+
+    /// https://www.mongodb.com/docs/manual/reference/command/hello
+    /// handshake https://github.com/mongodb/specifications/blob/master/source/auth/auth.md#authentication
+    fn hello(self: *@This()) !void {
+        // will expand to connection pool later
+        const stream = try std.net.tcpConnectToAddress(self.options.address);
+        defer stream.close();
+
+        // write request
+
+        var buf = std.ArrayList(u8).init(self.allocator);
+        defer buf.deinit();
+
+        var payload = buf.writer();
+        // msg header
+        try payload.writeInt(i32, 1, .little); // request id
+        try payload.writeInt(i32, 0, .little); // response to
+        try payload.writeInt(i32, OpCode.msg.toInt(), .little); // op code
+
+        // op msg
+        try payload.writeInt(u32, 0, .little); // flags
+        try payload.writeInt(u8, SectionKind.body.toInt(), .little);
+        var bsonBuf = std.ArrayList(u8).init(self.allocator);
+        defer bsonBuf.deinit();
+        var bsonWriter = bson.writer(self.allocator, bsonBuf.writer());
+        defer bsonWriter.deinit();
+        // hello command 
+        try bsonWriter.write(RawBson.document(&.{
+            .{ "hello", RawBson.int32(1) },
+            .{ "$db", RawBson.string("test") },
+        }));
+        const bsonBytes = try bsonBuf.toOwnedSlice();
+        defer self.allocator.free(bsonBytes);
+        try payload.writeAll(bsonBytes);
+
+        const requestBytes = try buf.toOwnedSlice();
+        defer self.allocator.free(requestBytes);
+        // msg len (including len bytes)
+        try stream.writer().writeInt(i32, @intCast(requestBytes.len + 4), .little);
+        try stream.writer().writeAll(requestBytes);
+
+        // todo optional CRC-32C checksum
+
+        // read response
+
+        const msgLen = try stream.reader().readInt(i32, .little);
+        const responseBuf = try self.allocator.alloc(u8, @intCast(msgLen - 4));
+        defer self.allocator.free(responseBuf);
+        _ = try stream.reader().readAll(responseBuf);
+        //std.debug.print(".msg len {d}\n", .{msgLen});
+        //std.debug.print("response bytes {any}\n", .{responseBuf});
+        var fbs = std.io.fixedBufferStream(responseBuf);
+        var responseReader = fbs.reader();
+        // header
+        std.debug.print("request id {d}\n", .{try responseReader.readInt(i32, .little)});
+        std.debug.print("response to {d}\n", .{try responseReader.readInt(i32, .little)});
+        // op code
+        switch (OpCode.fromInt(try responseReader.readInt(i32, .little))) {
+            .reply => {
+                std.debug.print("response flags  {d}\n", .{try responseReader.readInt(i32, .little)});
+                std.debug.print("cursor id {d}\n", .{try responseReader.readInt(i64, .little)});
+                std.debug.print("starting from {d}\n", .{try responseReader.readInt(i32, .little)});
+                std.debug.print("number returned {d}\n", .{try responseReader.readInt(i32, .little)});
+                var bsonReader = bson.reader(self.allocator, responseReader);
+                defer bsonReader.deinit();
+                const docs = try bsonReader.read();
+                //std.debug.print("documents {any}\n", .{docs});
+                // https://www.mongodb.com/docs/manual/reference/method/db.runCommand/#response
+                switch (docs) {
+                    .document => |v| {
+                        for (v.elements) |elem| {
+                            std.debug.print("document {s}", .{elem.@"0"});
+                            switch (elem.@"1") {
+                                .string => |s| std.debug.print(" {s}\n", .{s}),
+                                else => |otherwise| std.debug.print("{any}\n", .{otherwise}),
+                            }
+                        }
+                    },
+                    else => unreachable,
+                }
+            },
+            .msg => {
+                std.debug.print("response flags  {d}\n", .{try responseReader.readInt(u32, .little)});
+                switch (SectionKind.fromInt(try responseReader.readInt(u8, .little))) {
+                    .body => {
+                        var bsonReader = bson.reader(self.allocator, responseReader);
+                        defer bsonReader.deinit();
+                        const doc = try bsonReader.read();
+                        //std.debug.print("documents {any}\n", .{docs});
+                        // https://www.mongodb.com/docs/manual/reference/method/db.runCommand/#response
+                        switch (doc) {
+                            .document => |v| {
+                                for (v.elements) |elem| {
+                                    std.debug.print("\ndocument {s}", .{elem.@"0"});
+                                    switch (elem.@"1") {
+                                        .string => |s| std.debug.print(" {s}", .{s}),
+                                        else => |otherwise| std.debug.print(" {any}", .{otherwise}),
+                                    }
+                                }
+                            },
+                            else => unreachable,
+                        }
+                    },
+                    else => |otherwise| std.debug.print("section {s} not yet supported", .{otherwise}),
+                }
+            },
+            else => |v| std.debug.print("op code {s} not yet supported", .{v}),
+        }
+    }
+};
+
+// https://www.mongodb.com/docs/manual/reference/command/hello/#syntax
+test "hello world" {
+    var client = Client.init(std.testing.allocator, .{});
+    try client.hello();
+}
